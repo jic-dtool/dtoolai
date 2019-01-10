c9eb24108a1f3980cb4273f2fd3e7cbc8754071c
Matthew Hartley <mhartley@cantab.net>
2019-01-10 13:20:16 +0000

    Add new strategy for setting train/verify

--- scripts/set_train_verify.py
+++ scripts/set_train_verify.py
@@ -18,13 +18,30 @@ def annotate_from_relpath(dataset):
     dataset.put_overlay('usetype', usetype)
 
 
+
+def annotate_from_tp_selection(dataset):
+
+    validation_tps = [80, 120, 180, 220]
+    val_strs = [str(vtp) for vtp in validation_tps]
+
+    def find_type(idn):
+        if any(vstr in dataset.item_properties(idn)['relpath'] for vstr in val_strs):
+            return 'validation'
+        else:
+            return 'training'
+
+    usetype = {idn: find_type(idn) for idn in dataset.identifiers}
+
+    dataset.put_overlay('usetype', usetype)
+
+    
 @click.command()
 @click.argument('dataset_uri')
 def main(dataset_uri):
 
     dataset = DataSet.from_uri(dataset_uri)
-
-    annotate_from_relpath(dataset)
+    annotate_from_tp_selection(dataset)
+    # annotate_from_relpath(dataset)
 
 
 if __name__ == '__main__':
