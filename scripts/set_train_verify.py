49883d9ff5ae0cc5c581ef24bae898d7ad1184c0
Matthew Hartley <mhartley@cantab.net>
2019-01-14 12:47:37 +0000

    Added new train/verify setting method

--- scripts/set_train_verify.py
+++ scripts/set_train_verify.py
@@ -1,3 +1,6 @@
+import math
+import random
+
 import click
 
 from dtoolcore import DataSet
@@ -34,14 +37,33 @@ def annotate_from_tp_selection(dataset):
 
     dataset.put_overlay('usetype', usetype)
 
+
+def annotate_by_random_selection(dataset, frac_verify):
+
+    is_image = dataset.get_overlay("is_image")
+    mask_lookup = dataset.get_overlay("mask_ids")
+    image_idns = [idn for idn in dataset.identifiers if is_image[idn]]
+    n_verify = math.floor(frac_verify * len(image_idns))
+
+    verify_image_idns = random.sample(image_idns, n_verify)
+
+    usetype_overlay = {idn: 'training' for idn in dataset.identifiers}
+
+    for idn in verify_image_idns:
+        usetype_overlay[idn] = 'verification'
+        usetype_overlay[mask_lookup[idn]] = 'verification'
+
+    dataset.put_overlay('usetype', usetype_overlay)
+
     
 @click.command()
 @click.argument('dataset_uri')
 def main(dataset_uri):
 
     dataset = DataSet.from_uri(dataset_uri)
-    annotate_from_tp_selection(dataset)
+    # annotate_from_tp_selection(dataset)
     # annotate_from_relpath(dataset)
+    annotate_by_random_selection(dataset, 0.2)
 
 
 if __name__ == '__main__':
